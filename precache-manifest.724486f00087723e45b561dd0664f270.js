ce659f5b8f17d262bb4a9159b8e65e4cb5687ef0
selimaakid1 <selima.akid@yahoo.com>
2020-03-05 16:38:36 +0100

    Updates

---
+++ precache-manifest.724486f00087723e45b561dd0664f270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896609aa6d9384cb814f7fcfd5c0effe",
+    "url": "/TodoAppReact/index.html"
+  },
+  {
+    "revision": "f1e4927ed8e67e276680",
+    "url": "/TodoAppReact/static/css/main.243c5728.chunk.css"
+  },
+  {
+    "revision": "138ab884c7162b9c9b53",
+    "url": "/TodoAppReact/static/js/2.bc6d238c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/TodoAppReact/static/js/2.bc6d238c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e4927ed8e67e276680",
+    "url": "/TodoAppReact/static/js/main.ab465efd.chunk.js"
+  },
+  {
+    "revision": "fcd2cd0ce0440a07dc44",
+    "url": "/TodoAppReact/static/js/runtime-main.6f420b48.js"
+  }
+]);
